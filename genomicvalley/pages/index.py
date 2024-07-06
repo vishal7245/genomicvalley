9bcf366615101fd42d276597c8b320db15194028
vishal <vishalonceagain@gmail.com>
2024-07-06 18:25:06 +0530

    minor changes

--- genomicvalley/pages/index.py
+++ genomicvalley/pages/index.py
@@ -6,11 +6,9 @@ from genomicvalley.components.footer import footer
 from genomicvalley.components.navbar import navbar
 from genomicvalley.components.contact import contact_section
 from genomicvalley.components.services import services_section
-from genomicvalley.components.testimonials import testimonials_section
 from genomicvalley.components.image_gallery import image_gallery
 from genomicvalley.components.about_section import about_section
 from genomicvalley.components.hero import hero_section
-from genomicvalley.components.news_scroller import news_scroller
 
 
 @rx.page(title="Genomic Valley")
