907080d889c1e9b77a8fdd4837672cd67fed505d
vishal <vishalonceagain@gmail.com>
2024-07-17 22:01:17 +0530

    major changes

--- rxconfig.py
+++ rxconfig.py
@@ -3,4 +3,12 @@ import reflex as rx
 config = rx.Config(
     app_name="genomicvalley",
     db_url="sqlite:///reflex.db",
+    frontend_port=3000,
+    backend_port=8000,
 )
+
+# Enable the use of X-Forwarded-For header
+config.api.use_xforwarded_for = True
+
+# Create your app instance with this config
+app = rx.App(config=config)
