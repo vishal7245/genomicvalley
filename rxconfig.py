84ff955cd75624536e196ceb915e1fe5bca0ab15
vishal <vishalonceagain@gmail.com>
2024-09-12 13:42:01 +0530

    port change

--- rxconfig.py
+++ rxconfig.py
@@ -3,8 +3,8 @@ import reflex as rx
 config = rx.Config(
     app_name="genomicvalley",
     db_url="sqlite:///reflex.db",
-    frontend_port=3000,
-    backend_port=8000,
+    frontend_port=3001,
+    backend_port=8001,
     cors_allowed_origins=["*"],
 )
 
