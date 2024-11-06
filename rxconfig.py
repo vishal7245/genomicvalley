d46cc064bb74bc7bd26c4e105ec24fdd2ba41125
Vishal <vishalonceagain@gmail.com>
2024-11-06 19:29:12 +0000

    added docker

--- rxconfig.py
+++ rxconfig.py
@@ -1,8 +1,12 @@
 import reflex as rx
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+    
 config = rx.Config(
     app_name="genomicvalley",
-    db_url="sqlite:///reflex.db",
+    db_url= os.getenv("DATABASE_URL"),
     frontend_port=3001,
     backend_port=8001,
     cors_allowed_origins=["*"],
